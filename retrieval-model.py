da57e4662563ded4e1beabba90436b51e0b893cb
varadchoudhari <varad.choudhari@gmail.com>
2017-11-27 20:57:49 -0500

    bm25

--- retrieval-model.py
+++ retrieval-model.py
@@ -1,3 +1,3 @@
 # Using BM25 retrieval model
 # Those who have reached here with the hope that you'll get a readymade code
-# for your upcoming IR assignment, hard luck!
+# for your upcoming IR assignment, hard luck! :D
