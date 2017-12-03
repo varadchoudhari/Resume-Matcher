08989496dcc8da2d1783adbc45a7fa5109c7370b
varadchoudhari <varad.choudhari@gmail.com>
2017-12-02 23:27:11 -0500

    fixed bugs

--- ranker.py
+++ ranker.py
@@ -2,12 +2,6 @@
 
 from math import log
 
-'''
-Using the following formula to calculate BM25
-((k3 + 1)q)/((k3 + q)) * ((k1 + 1)f)/((K + f)) * log((r + 0.5)(N − n − R + r + 0.5))/((n − r + 0.5)(R − r + 0.5))
-REFERENCE: https://xapian.org/docs/bm25.html
-'''
-
 # DEFINING CONSTANTS
 
 k1 = 1.2
