65b40b2021eec98190920c9d769fa4cae230a233
varadchoudhari <varad.choudhari@gmail.com>
2017-12-15 09:07:34 +0530

    implementing BM25

--- ranker.py
+++ ranker.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from math import log
 
-k1 = 1.2
-b = 0.75
-k2 = 100
-R = 0 
+'''
+Using the following formula to calculate BM25
+((k3 + 1)q)/((k3 + q)) * ((k1 + 1)f)/((K + f)) * log((r + 0.5)(N − n − R + r + 0.5))/((n − r + 0.5)(R − r + 0.5))
+REFERENCE: https://xapian.org/docs/bm25.html
+'''
 
-
-def BM25(dl, avdl, N, n, r, q, f):
