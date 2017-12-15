498ad7362b189cda066650f371854a66ba8a97d4
varadchoudhari <varad.choudhari@gmail.com>
2017-12-15 09:08:52 +0530

    implenting BM25

--- ranker.py
+++ ranker.py
@@ -15,3 +15,11 @@ b = 0.75
 k2 = 100
 R = 0 #Since no relevance info is available
 
+# MAIN METHOD
+
+def BM25(docLen, avDocLen, n, N, f, q, r):
+    p1 = ((k2 + 1) * q) / (k2 + q)
+    p2 = ((k1 + 1) * f) / (getK(docLen, avDocLen) + f)
+    p3 = log((r + 0.5) * (N - n - R + r + 0.5)) / ((n - r + 0.5) * (R - r + 0.5))
+    return p1 * p2 * p3
+
